3768159986f46d8ba9b87fb8bf5fe4dbcdf58b63
RAVI <rkbansal83@gmail.com>
2016-04-13 00:28:43 -0700

    algorithms

--- src/algos/BinaryMinHeap.java
+++ src/algos/BinaryMinHeap.java
@@ -1,66 +1,46 @@
 package algos;
 
-public class BinaryMinHeap {
+public class BinaryMinHeap<T extends Comparable<T>> {
 	
-	private int[] data;
-	public int[] getData() {
+	private T[] data;
+	private int heapSize;
+	public T[] getData() {
 		return data;
 	}
-
-	public void setData(int[] data) {
-		this.data = data;
-	}
-	private int heapSize;
-
 	public int getHeapSize() {
 		return heapSize;
 	}
-
-	public void setHeapSize(int heapSize) {
-		this.heapSize = heapSize;
-	}
-
 	public BinaryMinHeap(int size) {
-		data = new int[size];
+		data =(T[]) new Comparable[size];
 		heapSize = 0;
 	}
-
-	public int getMinimum() {
+	public T getMinimum() {
 		if (isEmpty())
-			throw new HeapException("Heap is empty");
+			throw new RuntimeException("Heap is empty");
 		else
 			return data[0];
 	}
-
-
 	public boolean isEmpty() {
         return (heapSize == 0);
-  }
+	}
 	private int getParentIndex(int nodeIndex) {
 		return (nodeIndex - 1) / 2;
 	}
-
-	public class HeapException extends RuntimeException {
-		public HeapException(String message) {
-			super(message);
-		}
-	}
-
-	public void insert(int value) {
+	public void insert(T value) {
 		if (heapSize == data.length)
-			throw new HeapException("Heap's underlying storage is overflow");
+			throw new RuntimeException("Heap's underlying storage is overflow");
 		else {
 			heapSize++;
 			data[heapSize - 1] = value;
 			shiftUp(heapSize - 1);
 		}
 	}
-
 	private void shiftUp(int nodeIndex) {
-		int parentIndex, tmp;
+		int parentIndex;
+		T tmp;
 		if (nodeIndex != 0) {
 			parentIndex = getParentIndex(nodeIndex);
-			if (data[parentIndex] > data[nodeIndex]) {
+			if (data[parentIndex].compareTo(data[nodeIndex])>0) {
 				tmp = data[parentIndex];
 				data[parentIndex] = data[nodeIndex];
 				data[nodeIndex] = tmp;
@@ -68,59 +48,48 @@ public class BinaryMinHeap {
 			}
 		}
 	}
-	
-	
 	public void removeMin() {
 		if (heapSize == 0)
-			throw new HeapException("Heap is Empty");
+			throw new RuntimeException("Heap is Empty");
 		else {
 			data[0] = data[heapSize - 1] ;
+			data[heapSize-1] = null;
 			heapSize--;
 			sinkDown(0);
 		}
 	}
-	
 	private void sinkDown(int nodeIndex) {
 		int left = nodeIndex*2+1;
 		int right = nodeIndex*2+2;
 		int min = nodeIndex;
-		if(left<heapSize && data[left]<data[min])
+		if(left<heapSize && data[left].compareTo(data[min])<0)
 			min = left;
-		if(right<heapSize && data[right]<data[min])
+		if(right<heapSize && data[right].compareTo(data[min])<0)
 			min = right;
 		
 		if(min!=nodeIndex){
-			int temp = data[min];
+			T temp = data[min];
 			data[min] = data[nodeIndex];
 			data[nodeIndex] = temp;
 			sinkDown(min);
 		}
 		
 	}
-	
-	
-	public static void display(BinaryMinHeap minHeap){
-		int[] arr = minHeap.getData();
-		
+	public static <T> void display(BinaryMinHeap minHeap){
+		T[] arr = (T[]) minHeap.getData();
 		for(int i=0;i<minHeap.getHeapSize();i++)
-			System.out.print(arr[i]+" ");
+			System.out.print(arr[i]+"->");
 	}
 	public static void main(String[] args){
-		
 		BinaryMinHeap minHeap = new BinaryMinHeap(5);
-
 		minHeap.insert(5);
 		minHeap.insert(2);
 		minHeap.insert(3);
 		minHeap.insert(4);
 		minHeap.insert(10);
-		
-		display(minHeap);
-		
+		display(minHeap);	
 		minHeap.removeMin();
-		
-		System.out.println("\nRemoved Min");
-		
+		System.out.println("\nRemoved Min");	
 		display(minHeap);
 	}
 }
---
+++ src/algos/MergeSortedArrays.java
@@ -0,0 +1,69 @@
+package algos;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MergeSortedArrays {
+	
+	public static void main(String[] args) {
+		
+		int[] a1 = {1,3,5,7};
+		int[] a2 = {2,4,6,8};
+		int[] a3 = {11,12};
+		
+		MergeSortedArrays msa = new MergeSortedArrays();
+		List<Integer> result = msa.merge(new int[][] {a1,a2,a3});
+		
+		System.out.println(result);
+	}
+	
+	class ArrayEntry implements Comparable<ArrayEntry> {
+		Integer element;
+		
+		public Integer getElement() {
+			return element;
+		}
+
+		public ArrayEntry(int element, int index) {
+			this.element = element;
+			this.index = index;
+		}
+		int index;
+
+		public int getIndex() {
+			return index;
+		}
+
+		@Override
+		public int compareTo(ArrayEntry o) {
+			// TODO Auto-generated method stub
+			return  this.element.compareTo(o.element);
+		}
+
+		
+	}
+	
+	public List<Integer> merge(int[][] arr) {
+		List<Integer> result = new ArrayList<Integer>();
+		BinaryMinHeap<ArrayEntry> minHeap = new BinaryMinHeap<ArrayEntry>(arr.length);
+		int[] indexesProcessed = new int[arr.length];
+		for(int i=0;i<arr.length;i++) {
+			ArrayEntry entry = new ArrayEntry(arr[i][0],i);
+			minHeap.insert(entry);
+		}
+		while(!minHeap.isEmpty()) {
+			result.add(minHeap.getMinimum().getElement());
+			int arrayIdx = minHeap.getMinimum().getIndex();
+			minHeap.removeMin();
+			indexesProcessed[arrayIdx] ++ ;
+			
+			if(indexesProcessed[arrayIdx]<arr[arrayIdx].length){
+				ArrayEntry entry = new ArrayEntry(arr[arrayIdx][indexesProcessed[arrayIdx]],arrayIdx);
+				minHeap.insert(entry);
+			}
+		}
+		
+		return result;
+
+	}
+}
